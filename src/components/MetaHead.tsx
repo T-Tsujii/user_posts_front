a7418bd8b11951059611bbe9196767c27bb307da
T.Tsujii <ttakehisabt@gmail.com>
2021-08-17 22:53:22 +0900

    各ページをテンプレートを実装

---
+++ src/components/MetaHead.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+type Props = {
+  title: string;
+};
+
+const MetaHead: React.FC<Props> = (props) => {
+  return (
+    <Head>
+      <title>{props.title}</title>
+      <meta name='description' content='投稿アプリのサンプルです。' />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+  );
+};
+
+export { MetaHead };
