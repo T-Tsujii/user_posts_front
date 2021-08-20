117646f2676d0a6123cfc18086e82a12055581e0
T.Tsujii <ttakehisabt@gmail.com>
2021-08-20 22:52:23 +0900

    CSRの一覧表示機能を実装

--- src/pages/csr.tsx
+++ src/pages/csr.tsx
@@ -1,10 +1,38 @@
 import type { NextPage } from 'next';
+import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
+
 import { MetaHead } from '../components/MetaHead';
 import { Links } from '../components/Links';
 import styles from '../styles/Home.module.css';
+import { globalAxios, errorHandling } from '../service/globalAxios';
+
+type PostType = {
+  id: number;
+  username: string;
+  body: string;
+  createdAt: string;
+  updatedAt: string;
+};
 
 const Csr: NextPage = () => {
   const title = '投稿一覧(CSR)';
+
+  const [posts, setPosts] = useState<PostType[]>([]);
+  const fetchPosts = async () => {
+    try {
+      const response = await globalAxios.get('/posts');
+      const posts = response.data;
+      setPosts(posts);
+    } catch (error) {
+      errorHandling(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <div className={styles.container}>
       <MetaHead title={title} />
@@ -12,6 +40,14 @@ const Csr: NextPage = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>{title}</h1>
         <Links />
+        {posts.map((post) => (
+          <div key={post.id}>
+            <div>投稿者名: {post.username}</div>
+            <div>内容: {post.body}</div>
+            <div>投稿日時: {format(new Date(post.createdAt), 'yyyy/MM/dd HH:mm:ss')}</div>
+            <div>更新日時: {format(new Date(post.updatedAt), 'yyyy/MM/dd HH:mm:ss')}</div>
+          </div>
+        ))}
       </main>
     </div>
   );
