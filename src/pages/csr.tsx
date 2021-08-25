68f2f9919b46281dc24c278d44a048e01b01711e
T.Tsujii <ttakehisabt@gmail.com>
2021-08-25 21:43:18 +0900

    CSR一覧表示のパフォーマンス向上

--- src/pages/csr.tsx
+++ src/pages/csr.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import { useState, useEffect } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
 import { MetaHead } from '../components/MetaHead';
 import { errorHandling } from '../service/globalAxios';
@@ -13,18 +13,18 @@ const Csr: NextPage = () => {
   const title = '投稿一覧(CSR)';
 
   const [posts, setPosts] = useState<PostType[]>([]);
-  const fetchPosts = async () => {
+  const fetchPosts = useCallback(async () => {
     try {
       const posts = await fetchPostsService();
       setPosts(posts);
     } catch (error) {
       errorHandling(error);
     }
-  };
+  }, [])
 
   useEffect(() => {
     fetchPosts();
-  }, []);
+  }, [fetchPosts]);
 
   return (
     <div>
